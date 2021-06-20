c5c4a1aa60b82529648d978f211e89a046cd09f4
soundsXsweet <57301173+soundsXsweet@users.noreply.github.com>
2021-06-20 11:04:23 -0500

    CS-1 modified layouts, fixed basic styling, moved DiceRolling into Shell component

--- src/Components/Shell.js
+++ src/Components/Shell.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
+import DiceContainer from './DiceContainer';
+import "./Shell.css";
 
 class Shell extends Component {
 
+    addToLocalStorage(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
     render(){
         return(
-            <>
-
-            </>
+            <div>
+                <div className="background">
+                    <DiceContainer></DiceContainer>
+                </div>
+            </div>
         );
     }
 }
